8c087c97535928c52a1303a39e0aa19c79d45f9d
hainweb <hainofficialweb@gmail.com>
2024-07-21 14:28:35 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,7 +23,7 @@ const Home = () => {
         return;
       }
 
-      const URL = '${process.env.REACT_APP_BACKEND_URL}'/api/user-details; 
+      const URL = '${process.env.REACT_APP_BACKEND_URL}/api/user-details' ; 
       const response = await axios.get(URL, { withCredentials: true }); 
  
       dispatch(setUser(response.data.data)); 
