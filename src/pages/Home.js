ca98fd0daa9f101d51bc8183b5816c9283090df6
hainweb <hainofficialweb@gmail.com>
2024-07-21 16:42:07 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -69,23 +69,28 @@ const Home = () => {
  
   const basePath = location.pathname === '/'; 
   return ( 
-    <div className='grid lg:grid-cols-[300px,1fr] h-screen max-h-screen'> 
-      <section className={bg-white ${!basePath && "hidden"} lg:block}> 
-        <Sidebar /> 
-      </section> 
- 
-      {/message component/} 
-      <section className={${basePath && "hidden"}} > 
-        <Outlet /> 
-      </section> 
- 
-      <div className={justify-center items-center flex-col gap-2 hidden ${!basePath ? "hidden" : "lg:flex"}}> 
-        <div> 
-          <img src={logo} width={250} alt='logo' /> 
-        </div> 
-        <p className='text-lg mt-2 text-slate-500'>Select user to send message</p> 
-      </div> 
-    </div> 
+    <div className='grid lg:grid-cols-[300px,1fr] h-screen max-h-screen'>
+        <section className={`bg-white ${!basePath && "hidden"} lg:block`}>
+           <Sidebar/>
+        </section>
+
+        {/**message component**/}
+        <section className={`${basePath && "hidden"}`} >
+            <Outlet/>
+        </section>
+
+
+        <div className={`justify-center items-center flex-col gap-2 hidden ${!basePath ? "hidden" : "lg:flex" }`}>
+            <div>
+              <img
+                src={logo}
+                width={250}
+                alt='logo'
+              />
+            </div>
+            <p className='text-lg mt-2 text-slate-500'>Select user to send message</p>
+        </div>
+    </div>
   ) 
 } 
  
