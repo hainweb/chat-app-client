3e5efc7c7378d891d18c220097ffc1ea2034c66c
hainweb <hainofficialweb@gmail.com>
2024-07-21 08:46:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import toast, { Toaster } from 'react-hot-toast';
-import LoginPage from './pages/LoginPage';
-import HomePage from './pages/HomePage';
+import LoginPage from './pages/CheckPasswordPage';
+import HomePage from './pages/Home';
 import CheckPasswordPage from './pages/CheckPasswordPage';
 import PrivateRoute from './components/PrivateRoute';
 
