5dbe08e98aa6485e0013c5cf5ca2ab915d5b67d1
hainweb <hainofficialweb@gmail.com>
2024-07-21 12:36:09 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -38,7 +38,7 @@ const Home = () => {
 
   /***socket connection */
   useEffect(() => {
-    const socketConnection = io(process.env.REACT_APP_BACKEND_URL, {
+    const socketConnection = io( 'wss://chat-app-server-hn8e.onrender.com', {
       auth: {
         token: localStorage.getItem('token')
       },
