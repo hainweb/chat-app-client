0f46ecf3e228d12e026d2a6a5ed881116fd41c8c
hainweb <hainofficialweb@gmail.com>
2024-07-21 09:00:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import toast, { Toaster } from 'react-hot-toast';
-import LoginPage from './pages/CheckPasswordPage';
+
 import HomePage from './pages/Home';
 import CheckPasswordPage from './pages/CheckPasswordPage';
 import PrivateRoute from './components/PrivateRoute';
@@ -12,7 +12,7 @@ function App() {
       <Toaster />
       <Router>
         <Routes>
-          <Route path="/login" element={<LoginPage />} />
+         
           <Route path="/check-password" element={<CheckPasswordPage />} />
           <PrivateRoute path="/" element={<HomePage />} />
         </Routes>
